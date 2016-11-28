36000ef3b80f7e8ca3631672f805cce54a21033d
kgbercan <kgbercan@gmail.com>
2016-11-27 22:10:01 -0500

    sarah's changes
    got rid of "no" on see results page
    called answer() instead of getQuestions in start() function
    added slide animation to name
    added dance animation to buttons: it wiggles when hovering over all
    buttons except the yes and no buttons; only wiggles them when they are
    active
    added in temporary name "snappy snacks" (very temporary)
    added audio, no controls but will work on those if we have time tomorrow

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ aNum = 0;
 // sweet > healthy > prep > hot
 answers = {"true": {"true": {"true": {"true": "SNACK A: seasonal, healthy, prep, hot", "false": "SNACK B: seasonal, healthy, prep, cold"}, "false": {"true": "SNACK C: seasonal, healthy, no prep, hot", "false": "SNACK D: seasonal, healthy, no prep, cold"}},"false": {"true": {"true": "SNACK E: seasonal, unhealthy, prep, hot","false": "SNACK F: seasonal, unhealthy, prep, cold"}, "false": {"true": "SNACK G: seasonal, unhealthy, no prep, hot","false": "SNACK H: seasonal, unhealthy, no prep, cold"}},}, "false": {"true": {"true": {"true": "SNACK I: not seasonal, healthy, prep, hot", "false": "SNACK J: not seasonal, healthy, prep, cold"}, "false": {"true": "SNACK K: not seasonal, healthy, no prep, hot", "false": "SNACK L: not seasonal, healthy, no prep, cold"}}, "false": {"true": {"true": "SNACK M: not seasonal, unhealthy, prep, hot", "false": "SNACK N: not seasonal, unhealthy, prep, cold"}, "false": {"true": "SNACK O: not seasonal, unhealthy, no prep, hot", "false": "SNACK P: not seasonal, unhealthy, no prep, cold"}},},};
 
+// audio controls 	
+var music = document.getElementById('music');
 
+function playAudio() {
+	if (music.paused) {
+		music.play();
+		pButton.className = "";
+		pButton.className = "pause";
+	} else { 
+		music.pause();
+		pButton.className = "";
+		pButton.className = "play";
+	}
+}
 
 $(document).ready(function(){
    $("#start").click(start)
@@ -14,16 +27,17 @@ $(document).ready(function(){
       answer(false)
    })
    $("#results").click(function(){
-      getResults(answers);
+      getResults(answers);  
    })
 });
+
 
 function start(){
    $("#loading").removeClass("hidden");
    $("#start").addClass("hidden");
    $("#yes").removeClass("hidden");
    $("#no").removeClass("hidden");
-   getQuestions();
+   answer();
 }
 
 function answer(ans){
@@ -41,6 +55,7 @@ function getQuestions(){
       else {
          $("#question").html("You've completed the quiz.<br>")
          $("#yes").addClass("hidden")
+		 $("#no").addClass("hidden")
          $("#start").addClass("hidden")
          $("#results").removeClass("hidden")
       }
@@ -64,6 +79,7 @@ function saveAnswer(ans){
 
 function getResults(answers){
    //display answer
+   
    $("#results").addClass("hidden")
    $("#details").removeClass("hidden")
 }
